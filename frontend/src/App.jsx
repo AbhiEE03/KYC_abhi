88ac81911f7eb85fc19fa57da3452e69b771052a
Abhishek Kumar <abhisheknoni78@gmail.com>
2026-04-26 18:16:46 +0530

    Updated the paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,37 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './components/Login';
-import MerchantDashboard from './components/MerchantDashboard';
-import ReviewerDashboard from './components/ReviewerDashboard';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from "react-router-dom";
+import Login from "./components/Login";
+import MerchantDashboard from "./components/MerchantDashboard";
+import ReviewerDashboard from "./components/ReviewerDashboard";
 
 function App() {
-  return (
-    <Router>
-      <div className="min-h-screen bg-gray-50 font-sans text-gray-900">
-        <header className="bg-white shadow border-b border-gray-200">
-          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold leading-tight text-gray-900">
-              Playto Pay
-            </h1>
-          </div>
-        </header>
-        <main>
-          <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-            <Routes>
-              <Route path="/" element={<Navigate to="/login" replace />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/merchant" element={<MerchantDashboard />} />
-              <Route path="/reviewer" element={<ReviewerDashboard />} />
-            </Routes>
-          </div>
-        </main>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="min-h-screen bg-gray-50 font-sans text-gray-900">
+				<header className="bg-white shadow border-b border-gray-200">
+					<div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+						<h1 className="text-3xl font-bold leading-tight text-gray-900">
+							Playto KYC
+						</h1>
+					</div>
+				</header>
+				<main>
+					<div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+						<Routes>
+							<Route path="/" element={<Navigate to="/login" replace />} />
+							<Route path="/login" element={<Login />} />
+							<Route path="/merchant" element={<MerchantDashboard />} />
+							<Route path="/reviewer" element={<ReviewerDashboard />} />
+						</Routes>
+					</div>
+				</main>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
